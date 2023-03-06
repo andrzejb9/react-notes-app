85e4e7369f03b121d3f2a8e28b1e5738a43cc221
andrzejb9 <a.bankowski9@o2.pl>
2023-03-06 14:23:46 +0100

    react-notes-app

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Notes App
+
+Open [https://andrzejb9.github.io/react-notes-app](https://andrzejb9.github.io/react-notes-app)
---
+++ src/components/NotesList.js
@@ -0,0 +1,21 @@
+import Note from "./Note";
+import AddNote from "./AddNote";
+
+function NotesList({ notes, handleAddNote, handleDeleteNote }) {
+  return (
+    <div className="notes-list">
+      {notes.map((note, index) => (
+        <Note
+          key={index}
+          id={note.id}
+          text={note.text}
+          date={note.date}
+          handleDeleteNote={handleDeleteNote}
+        />
+      ))}
+      <AddNote handleAddNote={handleAddNote} />
+    </div>
+  );
+}
+
+export default NotesList;
